7ad5e89b085b1627076dab570afd17c1f32e4983
Skivel <46036051+Skivel@users.noreply.github.com>
2021-04-25 18:48:05 +0300

    beta

---
+++ Lab_16/index.php
@@ -0,0 +1,7 @@
+<?php echo "<form id='forma' action='script1.php' method='post'> 
+<h1>Форма входу</h1> 
+<p>Заповніть поля для входу на сайт</p> 
+<p>Логін<br /><input type='text' name='login'></p> 
+<p>Пароль<br /><input type='password' name='password'></p> 
+<p><input type='submit' name='submit' value='Ввійти'> <br></p></form>" 
+?>
